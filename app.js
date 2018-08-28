b8bfabca52a282f614d1511b4c312c7976754527
TRamos5 <travis.ramos5@gmail.com>
2018-08-28 13:08:49 -0500

    setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//test 
+let express = require('express');
+let app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+app.set('view engine', 'ejs');
+app.set('appData', data);
+
+app.use(express.static('public'));
+
+
+
+
+
+
+http.listen(9000, () =>{
+    console.log('listening port 9000');
+});
