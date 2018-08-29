2dcdb6f52e6392219135c05ac280df3a88e7e5e2
TRamos5 <travis.ramos5@gmail.com>
2018-08-28 21:20:00 -0500

    sequal trial

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ let express = require('express');
 let app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Sequelize = require('sequelize');
+var connnection = new Sequelize('project2', 'projectwo', 'jurassicpark', {
+    dialect: 'postgres'
+});
 
 
 
 app.set('view engine', 'ejs');
-app.set('appData', data);
+// app.set('appData', data);
 
 app.use(express.static('public'));
 
-
+app.get('/home', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home'
+    })
+});
 
 
 
@@ -18,3 +26,14 @@ app.use(express.static('public'));
 http.listen(9000, () =>{
     console.log('listening port 9000');
 });
+
+
+
+//questoin i have: why use migrate(slide 15) when you can just add in the code in the app.js file?
+
+// var Article = connection.define('article', {
+//     title: Sequelize.STRING,
+//     body: Sequelize.TEXT
+// });
+
+// connection.sync();
