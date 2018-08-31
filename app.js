fc108a62e751607411eec5b653d810f22fc458cc
TRamos5 <travis.ramos5@gmail.com>
2018-08-31 17:18:19 -0500

    input error

--- app.js
+++ app.js
@@ -6,43 +6,17 @@ var Sequelize = require('sequelize');
 let db = require('./models');
 
 
-
 app.set('view engine', 'ejs');
 // app.set('appData', data);
 
 app.use(express.static('public'));
 
-app.get('/home', (req, res) => {
-    res.render('home', {
-        pageTitle: 'Home'
-    })
-});
 
-//Test for tables
-// db.user.create({name:'Travis', userName:'moto', email:'me@me.com'})
-// db.user.create({name:'Hussein', userName:'dfdto', email:'me@me.com'})
-// db.user.create({name:'Eric', userName:'motdfad', email:'me@me.com'})
-// db.project.create({projectName:'p1', teamLead:'Travis'})
+app.use(require('./routes/login'));
+app.use(require('./routes/register'));
+app.use(require('./routes/dashboard'));
+app.use(require('./routes/logout'));
 
-
-db.user.findAll({
-    include: [{
-    model: db.project,
-    required: true
-    }]
-}).then(function(results){
-        results.forEach(function(record){
-            console.log(record.userName, record.project.projectName)
-        })
-    })
-
-
-// db.project.findAll()
-//     .then(function(results){
-//         results.forEach(function(record){
-//             console.log(record.projectName)
-//         })
-//     })
 http.listen(9000, () =>{
     console.log('listening port 9000');
 });
---
+++ routes/dashboard.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/dashboard',function(req,res){
+  
+    if(!req.isAuthenticated()) {
+        res.redirect('/login');
+      return
+    }
+  
+    res.send("You are logged in!")
+  })
+
+
+module.exports = router;
